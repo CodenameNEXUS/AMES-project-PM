cbe59153d2da49a4673ecf07c3ad91263d661b2c
ToastyKitty48 <26.Cooper.Lyon@ksd.org>
2025-04-08 13:50:24 -0700

    Adding Pause Menu Prefab and code

---
+++ Broken Pieces/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    Canvas canvas;
+    float timeScaleDef;
+    void Start()
+    {
+        timeScaleDef = Time.timeScale;
+        canvas = GetComponent<Canvas>();
+        canvas.enabled = false;
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) && canvas.enabled == false)
+        {
+            Pause();
+        } else if (Input.GetKeyDown(KeyCode.Escape) && canvas.enabled == true)
+        {
+            Resume();
+        }
+    }
+    public void Pause()
+    {
+        canvas.enabled = true;
+        Time.timeScale = 0;
+    }
+    public void Resume()
+    {
+        canvas.enabled = false;
+        Time.timeScale = timeScaleDef;
+    }
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
