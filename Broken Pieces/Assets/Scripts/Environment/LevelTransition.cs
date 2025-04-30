70432271ba3ebb7f0a0cc3cf6bde771a3742f0b4
ToastyKitty48 <26.Cooper.Lyon@ksd.org>
2025-04-30 13:01:32 -0700

    Make a fade to black on level transition

--- Broken Pieces/Assets/Scripts/Environment/LevelTransition.cs
+++ Broken Pieces/Assets/Scripts/Environment/LevelTransition.cs
@@ -2,13 +2,45 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LevelTransition : MonoBehaviour
 {
     [SerializeField] private int levelToLoad;
+    [SerializeField] private float fadeToBlackTime = 1;
+    private float timer = -2;
+    public Image blackBox;
+    private bool fadingToBlack = false;
+    private bool ran1 = false;
+    public void Start()
+    {
+        blackBox = gameObject.GetComponentInChildren<Image>();
+        blackBox.color = new Color(blackBox.color.r, blackBox.color.g, blackBox.color.b, 1f);
+    }
+    public void FixedUpdate()
+    {
+        timer -= Time.deltaTime;
+        if (timer > 0)
+        {
+            blackBox.color =  new Color(blackBox.color.r, blackBox.color.g, blackBox.color.b, blackBox.color.a + (0.03f/fadeToBlackTime));
+            if (!ran1)
+            {
+                ran1 = true;
+                blackBox.color = new Color(blackBox.color.r, blackBox.color.g, blackBox.color.b, 0f);
+            }
+        } else if ( timer < 0 && timer > -1)
+        {
+            SceneManager.LoadScene(levelToLoad);
+        }
+        if (!fadingToBlack)
+        {
+            blackBox.color = new Color(blackBox.color.r, blackBox.color.g, blackBox.color.b, blackBox.color.a - (0.03f / fadeToBlackTime));
+        }
+    }
     private void LoadLevel()
     {
-        SceneManager.LoadScene(levelToLoad);
+        fadingToBlack = true;
+        timer = fadeToBlackTime;
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
