7f2e9c040acf4ca5496f61f42552aab5f1729f91
ToastyKitty48 <26.Cooper.Lyon@ksd.org>
2025-04-30 13:59:03 -0700

    Working on getting player animations working

--- Broken Pieces/Assets/Scripts/PlayerRelated/PlayerAnimationControl.cs
+++ Broken Pieces/Assets/Scripts/PlayerRelated/PlayerAnimationControl.cs
@@ -19,7 +19,7 @@ public class PlayerAnimationControl : MonoBehaviour
     }
     void Update()
     {
-        /*if (playerRB.velocity.x > -0.1 && playerRB.velocity.x < 0.1)
+        if (playerRB.velocity.x > -0.1 && playerRB.velocity.x < 0.1)
         {
             playerVelX = 0;
         }
@@ -29,7 +29,14 @@ public class PlayerAnimationControl : MonoBehaviour
         } else if (playerRB.velocity.x > 0.1)
         {
             playerVelX = 1;
-        }*/
+        }
+        if (playerVelX == -1 || PlayerScript.wallHangR)
+        {
+            SPR.flipX = true;
+        } else if(playerVelX == 1 || PlayerScript.wallHangL)
+        {
+            SPR.flipX = false;
+        }
         if (playerRB.velocity.y > -0.1 && playerRB.velocity.y < 0.1)
         {
             playerVelY = 0;
@@ -41,9 +48,18 @@ public class PlayerAnimationControl : MonoBehaviour
         {
             playerVelY = 1;
         }
+        if (playerRB.velocity.x > 0)
+        {
+            //playerAC.speed = 1 + playerRB.velocity.x * 0.1f;
+            playerAC.SetFloat("AnimationSpeed", playerRB.velocity.x * 0.2f);
+        }
+        if (playerRB.velocity.x < 0)
+        {
+            //playerAC.speed = 1 + playerRB.velocity.x * -0.1f;
+            playerAC.SetFloat("AnimationSpeed", playerRB.velocity.x * -0.2f);
+        }
         playerAC.SetBool("IsGrounded", PlayerScript.isGrounded);
-        playerAC.SetBool("WallHangL", PlayerScript.wallHangL);
-        playerAC.SetBool("WallHangR", PlayerScript.wallHangR);
+        playerAC.SetBool("IsWallHanging", PlayerScript.haningOnWall);
         playerAC.SetInteger("VelocityX", playerVelX);
         playerAC.SetInteger("VelocityY", playerVelY);
     }
