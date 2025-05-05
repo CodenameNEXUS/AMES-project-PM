d787ab92ee7170dba7661a722dee28106c565260
ToastyKitty48 <26.Cooper.Lyon@ksd.org>
2025-05-05 13:46:27 -0700

    Getting UI animation working

---
+++ Broken Pieces/Assets/Scripts/UIImages.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIImages : MonoBehaviour
+{
+    public Sprite UIImage1;
+    public Sprite UIImage2;
+    public Sprite UIImage3;
+    public Sprite UIImage4;
+    public Sprite UIImage5;
+    public Sprite UIImage6;
+    public Sprite UIImage7;
+    public Sprite UIImage8;
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+}
